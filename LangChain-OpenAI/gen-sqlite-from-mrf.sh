4e207713bed3d750141454a348b96fea83e9da73
jazzwang <jazzwang.tw@gmail.com>
2024-06-24 03:05:44 +0000

    update gen-sqlite-from-mrf.sh due to Cyberattack

--- LangChain-OpenAI/gen-sqlite-from-mrf.sh
+++ LangChain-OpenAI/gen-sqlite-from-mrf.sh
@@ -1,10 +1,13 @@
+#####
+## 2024-06-24 Update: MRF website is no longer available due to CHC Cyberattach on Feb 21, 2024
+#####
 # DOWNLOAD
-wget https://assets.changehealthcare.com/Shop/PROD/static/BaconCountyHospital/ein_BaconCountyHospital_standardcharges.csv.zip
+# wget https://assets.changehealthcare.com/Shop/PROD/static/BaconCountyHospital/ein_BaconCountyHospital_standardcharges.csv.zip
 # UNZIP and SAMPLE
-unzip ein_BaconCountyHospital_standardcharges.csv.zip
+# unzip ein_BaconCountyHospital_standardcharges.csv.zip
 # REMOVE ZIP
-rm *.zip
+# rm *.zip
 # remove top 3 lines
-grep '","' 58-2224545_BaconCountyHospital_standardcharges.csv > sample.csv
+# grep '","' 58-2224545_BaconCountyHospital_standardcharges.csv > sample.csv
 sqlite3 sample.db < import.sql
 #rm *.csv
